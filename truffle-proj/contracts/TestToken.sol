59cf9655c4264700c3007cdefa4c6b9ae588ff29
Breedoon <breedoon@programmer.net>
2021-11-05 01:12:21 +0100

    Added a truffle project with a test token

---
+++ truffle-proj/contracts/TestToken.sol
@@ -0,0 +1,12 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenStreetTest is ERC20 {
+    constructor() ERC20("TokenStreetTest", "TST") {
+        _mint(msg.sender, 1000);
+    }
+}
+
